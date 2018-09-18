a19a9e38874498e5c81237c5becd3fdd8be8aa4f
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-18 15:39:53 +0100

    update encoding

--- src/components/Search/format.js
+++ src/components/Search/format.js
@@ -1,9 +1,9 @@
 // TODO add polyfil for text decoder
 import * as R from 'ramda';
+import { decode } from '../../encoding';
 import {
   locationSearch,
   textSearch,
-  splitWhere,
   markPositionalSearches,
   markTextSearches,
   combineEntries,
@@ -14,8 +14,8 @@ import {
 const NEW_LINE_LENGTH = 1;
 
 // Each row should return an array of entries, even if it's a single one
-const formatRow = decoder => ({ searches, formatted, startIndex }, row) => {
-  const text = decoder.decode(row);
+const formatRow = ({ searches, formatted, startIndex }, row) => {
+  const text = decode(row);
   const endIndex = startIndex + text.length + NEW_LINE_LENGTH;
   const [inRowSearches, otherSearches] = R.splitWhen(
     search => search.position.end > endIndex,
@@ -36,9 +36,8 @@ const formatRow = decoder => ({ searches, formatted, startIndex }, row) => {
 
 const formatText = initialSearches => {
   const initialState = { formatted: [], searches: initialSearches, startIndex: 0 };
-  const decoder = new TextDecoder('utf-8');
   return R.pipe(
-    R.reduce(formatRow(decoder), initialState),
+    R.reduce(formatRow, initialState),
     R.prop('formatted')
   );
 };
