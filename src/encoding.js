4a61577290c8f86fcfa1020b7e82fe5fc3228f1a
WhoAteDaCake <doitsimplefy@gmail.com>
2018-09-18 16:04:01 +0100

    import encoder

--- src/encoding.js
+++ src/encoding.js
@@ -1,9 +1,4 @@
-if (!('TextDecoder' in self)) {
-  import('text-encoding-utf-8').then(({ TextDecoder, TextEncoder }) => {
-    self.TextDecoder = TextDecoder;
-    self.TextEncoder = TextEncoder;
-  });
-}
+import { TextDecoder, TextEncoder } from 'text-encoding-utf-8';
 
 export const decode = value => new TextDecoder('utf-8').decode(value);
 export const encode = value => new TextEncoder('utf-8').encode(value);
