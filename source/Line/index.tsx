959cbdad32efd387ce51dd6472e4fc47eb85eaf5
Augustinas <augustinas@spotlightdata.co.uk>
2019-03-21 12:46:53 +0000

    export styles

--- source/Line/index.tsx
+++ source/Line/index.tsx
@@ -12,14 +12,28 @@ type Props = {
   data: Context;
 };
 
+type Styles = {
+  numberContainer: React.CSSProperties;
+  number: React.CSSProperties;
+};
+
+const styles: Styles = {
+  numberContainer: {
+    display: 'inline-block',
+    height: '100%',
+    padding: '0 0.5em',
+  },
+  number: {
+    float: 'right',
+  },
+};
+
 export const Line: React.FC<Props> = ({ index, style, data }) => {
   const { numberWidth, getRowText } = data;
   return (
     <div style={{ ...style, display: 'flex' }}>
-      <div
-        style={{ width: numberWidth, display: 'inline-block', height: '100%', padding: '0 0.5em' }}
-      >
-        <span style={{ float: 'right' }}>{index}</span>
+      <div style={{ ...styles.numberContainer, width: numberWidth }}>
+        <span style={styles.number}>{index}</span>
       </div>
       <div style={{ width: `calc(100% - ${numberWidth}px)`, display: 'inline-block' }}>
         <span>{getRowText(index)}</span>
