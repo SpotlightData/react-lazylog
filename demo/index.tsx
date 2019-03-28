9e8e086fc4da915b275dd1a5a94b0541ea24bdcd
Augustinas <augustinas@spotlightdata.co.uk>
2019-03-28 10:56:52 +0000

    pass height props

--- demo/index.tsx
+++ demo/index.tsx
@@ -4,6 +4,12 @@ import { text } from './demoText';
 import { DocumentViewer } from '../source/DocumentViewer';
 
 render(
-  <DocumentViewer text={text.split('\n')} document={document} rulerId="ruler" width={400} />,
+  <DocumentViewer
+    text={text.split('\n')}
+    document={document}
+    rulerId="ruler"
+    width={400}
+    height={600}
+  />,
   document.getElementById('root')
 );
